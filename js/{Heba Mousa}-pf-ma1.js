6f962f9cd3be4491ceb5f288fb317885a8bb687c
Heba-9183 <79254938+Heba-9183@users.noreply.github.com>
2021-05-30 20:21:09 +0200

    creating js files

---
+++ js/{Heba Mousa}-pf-ma1.js
@@ -0,0 +1,32 @@
+/** @format */
+// question 1
+var age = 15;
+var studentname = "Sara";
+var status = true;
+console.log(age);
+console.log(studentname);
+console.log(status);
+
+// question 2
+var firstname = "Heba";
+var lastname = "Mousa";
+var name = firstname + " " + lastname;
+console.log(name);
+
+// question 3
+x = typeof "frog";
+console.log("The type of 'frog' is " + x);
+
+// question 4
+var orderHasShipped = true;
+if (orderHasShipped == true) {
+  console.log("The order shipped");
+} else {
+  console.log("The order did not ship");
+}
+
+// question 5
+
+for (var i = 7; i < 14; i++) {
+  console.log(i);
+}
